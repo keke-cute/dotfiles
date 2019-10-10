04c325d51c56962d5c11af26a22c56d3a5ba05d8
Liu Shike <liushike1997@gmail.com>
2019-10-10 20:57:57 +0800

    add fish conf

---
+++ .config/fish/functions/emacs.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function emacs --description 'alias emacs emacs -nw'
+	command emacs -nw $argv;
+end
---
+++ .config/fish/functions/xi.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function xi --description 'alias xi sudo xbps-install'
+	sudo xbps-install $argv;
+end
