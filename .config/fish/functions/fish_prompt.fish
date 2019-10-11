79a1c5f119fce0a3fd9b4213aaff820e7e6a446a
Liu Shike <liushike1997@gmail.com>
2019-10-11 17:25:36 +0800

    update fish

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,33 +1,67 @@
 function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
+    set -l normal (set_color normal)
 
-    # User
-    set_color $fish_color_user
-    echo -n (whoami)
-    set_color normal
+    # Hack; fish_config only copies the fish_prompt function (see #736)
+    if not set -q -g __fish_classic_git_functions_defined
+        set -g __fish_classic_git_functions_defined
 
-    echo -n '@'
+        function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
+            if status --is-interactive
+                commandline -f repaint 2>/dev/null
+            end
+        end
 
-    # Host
-    set_color $fish_color_host
-    echo -n (prompt_hostname)
-    set_color normal
+        function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
+            if status --is-interactive
+                commandline -f repaint 2>/dev/null
+            end
+        end
 
-    echo -n ':'
+        function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
+            if status --is-interactive
+                commandline -f repaint 2>/dev/null
+            end
+        end
 
-    # PWD
-    set_color $fish_color_cwd
-    echo -n (prompt_pwd)
-    set_color normal
+        function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
+            if status --is-interactive
+                commandline -f repaint 2>/dev/null
+            end
+        end
 
-    __terlar_git_prompt
-    __fish_hg_prompt
-    echo
-
-    if not test $last_status -eq 0
-        set_color $fish_color_error
+        # initialize our new variables
+        if not set -q __fish_classic_git_prompt_initialized
+            set -qU fish_color_user
+            or set -U fish_color_user -o green
+            set -qU fish_color_host
+            or set -U fish_color_host -o cyan
+            set -qU fish_color_status
+            or set -U fish_color_status red
+            set -U __fish_classic_git_prompt_initialized
+        end
     end
 
-    echo -n '➤ '
-    set_color normal
+    set -l color_cwd
+    set -l prefix
+    set -l suffix
+    switch "$USER"
+        case root toor
+            if set -q fish_color_cwd_root
+                set color_cwd $fish_color_cwd_root
+            else
+                set color_cwd $fish_color_cwd
+            end
+            set suffix '#'
+        case '*'
+            set color_cwd $fish_color_cwd
+            set suffix '>'
+    end
+
+    set -l prompt_status
+    if test $last_status -ne 0
+        set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
+    end
+
+    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_vcs_prompt) $normal $prompt_status $suffix " "
 end
