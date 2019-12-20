4dbdd73496df864232840a584f38d4159a7b1d93
Liu Shike <liushike1997@gmail.com>
2019-12-20 15:38:43 +0800

    update dwmbar

---
+++ scripts/dwm-status-refresh.sh
@@ -0,0 +1,17 @@
+print_hello()
+{
+    echo "Have A Good Time,$USER! "
+}
+print_date(){
+    date '+%Y-%m-%d %H:%M:%S'
+}
+print_mem()
+{
+    free -h | awk '/Mem:/ { printf("%s/%s\n",$3,$2)}'
+}
+print_disk()
+{
+    df -h / | awk '/\//{ printf("%4s/%s\n",$4,$2)}'
+}
+xsetroot -name "$(print_mem)$(print_disk) $(print_date)"
+exit 0
