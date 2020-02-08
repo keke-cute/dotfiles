e3b25495237f28479df905232a4b6a3b9d59cf11
Liu Shike <liushike1997@gmail.com>
2020-02-08 17:02:16 +0800

    fust push

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,13 +1,11 @@
-------------------------------------------------------------------------
----IMPORTS
-------------------------------------------------------------------------
     -- Base
 import XMonad
-import XMonad.Config
     -- Utilities
 import XMonad.Util.EZConfig (additionalKeysP, additionalMouseBindings)
+import XMonad.Util.SpawnOnce
     -- Hooks
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.SetWMName
     -- Layouts modifiers
 import XMonad.Layout.Spacing (spacing) 
 -- The main function.
@@ -26,12 +24,17 @@ toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 myConfig = defaultConfig
   {
     modMask = mod4Mask
+  , startupHook = myStartupHook
   , terminal = "st"
   , borderWidth = 2
   , normalBorderColor  = "#292d3e"
   , focusedBorderColor = "#bbc5ff"
   , layoutHook = spacing 2 $ Tall 1 (3/100) (1/2)
   }  `additionalKeysP`         myKeys 
+myStartupHook = do
+          spawnOnce "picom &" 
+          spawnOnce "feh --bg-fill ~/Downloads/wall.png &"
+          setWMName "LG3D"
 myKeys =
         [
           ("M-<Space>", spawn "rofi -font \"Operator Mono Book 15\" -combi-modi window,drun,ssh,run -theme lb -show combi")
