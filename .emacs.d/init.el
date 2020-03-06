3828dc03533adccaec28dd26a9f9df42a91b2c75
Liu Shike <liushike1997@gmail.com>
2020-03-06 18:25:41 +0800

    update emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,28 +1,3 @@
-;; 加载外部配置文件夹
-(package-initialize)
-(add-to-list 'load-path "~/.emacs.d/config/")
-
-;; 初始化 straight.el 插件管理
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-;; 预下载基础包
-(straight-use-package 'use-package)
-
-;; 加载外部的配置文件
-(require 'packages)
-;;(require 'pyim)
-(require 'basic)
-;;(require 'lang)
-(require 'run)
-(require 'keybind)
+(require 'org-install)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "myemacsconf.org" user-emacs-directory))
