3513ee4e97338dd7b01a0015acc4c774e525e36a
Liu Shike <liushike1997@gmail.com>
2020-03-14 13:17:22 +0800

    add rime conf

--- rime/rime.sh
+++ rime/rime.sh
@@ -1,4 +1,4 @@
 echo "sync_dir: '/Users/keke/dotfiles/rime'" >> installation.yaml
+cp /Users/keke/dotfiles/rime/73217228-8b36-462c-bf9c-25ab9ccc5f7d/double_pinyin_flypy.schema.yaml /Users/keke/Library/Rime/double_pinyin_flypy.schema.yaml
 
-
-echo "installation_id: 'keke'" >> installation.yaml
+cp /Users/keke/dotfiles/rime/73217228-8b36-462c-bf9c-25ab9ccc5f7d/default.custom.yaml /Users/keke/Library/Rime/default.custom.yaml
