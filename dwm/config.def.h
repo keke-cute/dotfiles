ed1e3ef0674c8ff6860e966d8de3cbe97426013c
Liu Shike <liushike1997@gmail.com>
2020-03-22 18:07:01 +0800

    update dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -67,12 +67,13 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *rofi[] = { "rofi", "-combi-modi", "window,drun,ssh,run", "-theme", "solarized", "-show", "combi", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+//static const char *rofi[] = { "rofi", "-combi-modi", "window,drun,ssh,run", "-theme", "solarized", "-show", "combi", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_space,  spawn,          {.v = rofi } },
+	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
