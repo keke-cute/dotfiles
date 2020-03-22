4644ce78941806e1c0a9b05908b67eaf57353fb9
Liu Shike <liushike1997@gmail.com>
2020-03-22 21:00:12 +0800

    fix dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -11,6 +11,7 @@ static const int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Operator Mono Book:size=14" };
+static const char dmenufont[]       = "Operator Mono Book:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
