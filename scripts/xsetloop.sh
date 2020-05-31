e1f0b751f056a5e42e666b9d84af585514ec8022
Liu Shike <liushike1997@gmail.com>
2020-05-31 13:36:45 +0800

    add all

--- scripts/xsetloop.sh
+++ scripts/xsetloop.sh
@@ -3,7 +3,7 @@
 let loop=0
 while true; do
 	if [[ $loop%300 -eq 0 ]]; then
-		weather=$(curl wttr.in/heze?format=1)
+		weather=$(curl wttr.in/juye?format=1)
 		let loop=$loop+1
 	fi
 	xsetroot -name " $weather | $(date '+%b %d %a') | $(date '+%H:%M') "
