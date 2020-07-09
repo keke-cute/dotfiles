9dea6fed28ad3cc1676202d2618acb0aac4bf805
Liu Shike <liushike1997@gmail.com>
2020-07-10 07:43:12 +0800

    add wx.sh

--- bin/wx.sh
+++ bin/wx.sh
@@ -1,1 +1,1 @@
-exec deepin-wine5 /home/keke/.wine/drive_c/Program\ Files/Tencent/WeChat/WeChat.exe
+export LANG=zh_CN.UTF-8 && exec deepin-wine5 /home/keke/.wine/drive_c/Program\ Files/Tencent/WeChat/WeChat.exe
