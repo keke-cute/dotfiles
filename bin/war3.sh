5d023e14f6f125a0ab3d89a5ac3448011cfad982
Liu Shike <liushike1997@gmail.com>
2020-07-11 14:40:14 +0800

    add all files

---
+++ bin/war3.sh
@@ -0,0 +1,1 @@
+export LANG=zh_CN.UTF-8 && exec deepin-wine5 /home/keke/games/Warcraft\ III/War3.exe -opengl
---
+++ bin/xunlei.sh
@@ -0,0 +1,1 @@
+export LANG=zh_CN.UTF-8 && exec deepin-wine5 /home/keke/.wine/drive_c/Program\ Files/Thunder\ Network/Thunder9/Program/thunderstart.exe
